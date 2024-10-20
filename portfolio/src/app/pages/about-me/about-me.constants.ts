125a78e88fb424bad905f38a64c3b99a268e112d
michaelvbend <brenzodj@gmail.com>
2024-10-20 18:13:46 +0200

    feat(): added about me page

---
+++ portfolio/src/app/pages/about-me/about-me.constants.ts
@@ -0,0 +1,18 @@
+export const skills = [
+  { name: 'Java', color: '#b07219' },
+  { name: 'Spring', color: '#6db33f' },
+  { name: 'Angular', color: '#dd0031' },
+  { name: 'React Native', color: '#61dafb' },
+  { name: 'Python', color: '#306998' },
+  { name: 'JavaScript', color: '#f7df1e' },
+  { name: 'TypeScript', color: '#3178c6' },
+  { name: 'Node.js', color: '#68a063' },
+  { name: 'SQL', color: '#00758f' },
+  { name: 'Docker', color: '#2496ed' },
+  { name: 'Kubernetes', color: '#326ce5' },
+  { name: 'AWS', color: '#ff9900' },
+  { name: 'Azure', color: '#0078d4' },
+];
+
+export const currentRole = 'Full-stack Developer';
+export const currentEmployer = 'Rabobank';

add03ccba244a12eb50b1298ac7f41385ca642bc
michaelvbend <brenzodj@gmail.com>
2024-10-24 19:54:41 +0200

    feat(): added reactive form to contact page

--- portfolio/src/app/pages/contact/contact.component.ts
+++ portfolio/src/app/pages/contact/contact.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
   email = 'mvanderbend@gmail.com';
   linkedin = 'https://www.linkedin.com/in/michael-vd-bend/';
   github = 'https://github.com/michaelvbend';
+
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    company: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+  });
+
+  // TODO: CHECKOUT https://www.youtube.com/watch?v=mOYAB1uMyhQ
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('test');
+      console.log(this.contactForm.value);
+    }
+  }
 }

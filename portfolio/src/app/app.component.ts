11176a56fe72435c603615d648a09d39736cd0b0
michaelvbend <brenzodj@gmail.com>
2024-12-05 18:18:45 +0100

    fix(analytics): add @vercel/analytics package and integrate into app component

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './shared/top-bar/top-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-
+import { inject } from '@vercel/analytics';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +10,8 @@ import { FooterComponent } from './shared/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    inject();
+  }
+}
